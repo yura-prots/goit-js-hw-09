0c2f32f6567d843ad1744b06f24a01b1ab703b4c
yura-prots <yuraprots@gmail.com>
2023-12-20 23:54:13 +0200

    add task-01 V1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let interval = null;
+
+refs.startBtn.addEventListener('click', () => setRandomBG());
+
+refs.stopBtn.addEventListener('click', () => stopRandomBG());
+
+function setRandomBG() {
+  interval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+}
+
+function stopRandomBG() {
+  clearInterval(interval);
+
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
