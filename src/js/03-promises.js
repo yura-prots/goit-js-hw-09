ca2cbfa4fac5301ee0bfd4ab33fc5a5618a3a26a
yura-prots <yuraprots@gmail.com>
2023-12-23 15:18:24 +0200

    task-3, add Notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,17 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  return console.log(e);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
