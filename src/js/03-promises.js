ffc71cd5b321ac62fc27ee8db6969ff71cb8e103
yura-prots <yuraprots@gmail.com>
2023-12-23 16:08:18 +0200

    task-3, add createPromise( )

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,19 +9,29 @@ refs.form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
 
-  const firstDelay = e.target.elements.delay.value;
-  const delayStep = e.target.elements.step.value;
-  const amount = e.target.elements.amount.value;
+  const firstDelay = Number(e.target.elements.delay.value);
+  const delayStep = Number(e.target.elements.step.value);
+  const amount = Number(e.target.elements.amount.value);
 
   return;
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    if (shouldResolve) {
+      resolve(position, delay);
+    } else {
+      reject(position, delay);
+    }
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
