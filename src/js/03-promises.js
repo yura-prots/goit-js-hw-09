c66176f4e95da29632060a508627f0e8cbba2107
yura-prots <yuraprots@gmail.com>
2023-12-23 18:46:14 +0200

    task-3, add setInterval( ) to promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,25 +13,27 @@ function onSubmit(e) {
   const delayStep = Number(e.target.elements.step.value);
   const amount = Number(e.target.elements.amount.value);
 
-  return;
+  createPromise(2, firstDelay);
 }
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
-    if (shouldResolve) {
-      resolve(position, delay);
-    } else {
-      reject(position, delay);
-    }
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-}
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
