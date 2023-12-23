5453846308c22c508f7b94faabe8aec5f30189bb
yura-prots <yuraprots@gmail.com>
2023-12-23 19:00:04 +0200

    task-3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -34,9 +34,9 @@ function createPromise(position, delay) {
 
   promise
     .then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
     });
 }
